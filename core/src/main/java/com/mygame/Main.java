c49acce8aa592655e49712717fb0636ea21e6394
romangoldz <roman.goldzitskiy.2011@gmail.com>
2026-08-16 12:17:18 +0300

    Fix import statement for Sprite class

--- core/src/main/java/com/mygame/Main.java
+++ core/src/main/java/com/mygame/Main.java
@@ -5,7 +5,7 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.Sprite;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 
 public class Main extends ApplicationAdapter {
   Texture imageTexture;
