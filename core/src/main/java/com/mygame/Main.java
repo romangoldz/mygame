0d79e4afd0206c0ead040b410b2a0c49f27357eb
romangoldz <roman.goldzitskiy.2011@gmail.com>
2026-08-17 20:39:39 +0300

    Implement button touch interaction for image rendering
    Added button interaction to render image on touch.

--- core/src/main/java/com/mygame/Main.java
+++ core/src/main/java/com/mygame/Main.java
@@ -6,35 +6,50 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.math.Rectangle;
 
 public class Main extends ApplicationAdapter {
-  Texture imageTexture;
-  Sprite image;
-  SpriteBatch batch;
-  
-  @Override
-  public void create(){
-    imageTexture = new Texture("images.png");
-    image = new Sprite(imageTexture);
-    image.setPosition(100, 100);
-    image.setSize (64, 64);
 
-    batch = new SpriteBatch();
-  }
+    Texture imageTexture;
+    Sprite image;
+    SpriteBatch batch;
+    Rectangle button;
 
-  @Override
-  public void render() {
-    Gdx.gl.glClearColor(0, 0, 0, 1);
-    Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-    
-    batch.begin();
-    image.draw(batch);
-    batch.end();
-  }
+    @Override
+    public void create() {
+        imageTexture = new Texture("images.png");
 
-  @Override
-  public void dispose() {
-    imageTexture.dispose();
-    batch.dispose();
-  }
+        image = new Sprite(imageTexture);
+        image.setPosition(100, 100);
+        image.setSize(64, 64);
+
+        button = new Rectangle(300, 200, 200, 100);
+
+        batch = new SpriteBatch();
+    }
+
+    @Override
+    public void render() {
+        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        batch.begin();
+
+        if (Gdx.input.isTouched()) {
+            float x = Gdx.input.getX();
+            float y = Gdx.input.getY();
+
+            if (button.contains(x, y)) {
+                image.draw(batch);
+            }
+        }
+
+        batch.end();
+    }
+
+    @Override
+    public void dispose() {
+        imageTexture.dispose();
+        batch.dispose();
+    } 
 }
