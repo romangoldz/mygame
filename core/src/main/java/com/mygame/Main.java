6cfc8c5796f2fcf783ebb600eef26cf413fff1ff
romangoldz <roman.goldzitskiy.2011@gmail.com>
2026-08-18 13:58:45 +0300

    Fix import statement for OrthographicCamera

--- core/src/main/java/com/mygame/Main.java
+++ core/src/main/java/com/mygame/Main.java
@@ -7,7 +7,7 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.math.Rectangle;
-import com badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 
 public class Main extends ApplicationAdapter {
 
