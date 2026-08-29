13806798c556ef6bf2a155fe5aface3ede71bcfa
romangoldz <roman.goldzitskiy.2011@gmail.com>
2026-08-29 11:17:45 +0300

    Refactor Main.java with improved input handling

--- core/src/main/java/com/mygame/Main.java
+++ core/src/main/java/com/mygame/Main.java
@@ -2,13 +2,16 @@ package com.mygame;
 
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.Sprite;        // ← Ось тут виправлення!
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.audio.Sound;
+import com.badlogic.gdx.graphics.Sprite;
+import com.badlogic.gdx.math.Vector2;
 
 public class Main implements ApplicationListener {
     Texture bgTexture;
@@ -20,19 +23,21 @@ public class Main implements ApplicationListener {
     
     SpriteBatch spriteBatch;
     FitViewport viewport;
+    Vector2 touchPos;
     
     @Override
     public void create() {
         bgTexture = new Texture("textures/bgTexture.png");
         bucketTexture = new Texture("textures/bucketTexture.png");
-        dropTexture = new Texture("textures/dropTexture.png");
+        dropTexture = new Texture("textures/dropTexture.png"); // додайте .png
         dropSound = Gdx.audio.newSound(Gdx.files.internal("sound/dropSound.mp3"));
         music = Gdx.audio.newMusic(Gdx.files.internal("music/music.mp3"));
         spriteBatch = new SpriteBatch();
         viewport = new FitViewport(8, 5);
-        music.play();
-        bucketSprite = new Sprite(bucketTexture);
+        music.play(); // якщо хочете одразу запустити музику
+        bucketSprite = new Sprite (bucketTexture);
         bucketSprite.setSize(1, 1);
+        touchPos = new Vector2();
     }
     
     @Override
@@ -50,7 +55,16 @@ public class Main implements ApplicationListener {
         draw();
     }
     
-    private void input() {}
+    private void input() {
+      float speed = 4f;
+      float delta = Gdx.graphics.getDeltaTime();
+
+      if (Gdx.input.isTouched()) {
+        touchPos.set(Gdx.input.getX(), Gdx.input.getY());
+        viewport.unproject(touchPos);
+        bucketSprite.setCenterX(touchPos.x);
+      }
+    }
     
     private void logic() {}
     
@@ -61,9 +75,8 @@ public class Main implements ApplicationListener {
 
         float worldWidth = viewport.getWorldWidth();
         float worldHeight = viewport.getWorldHeight();
-        spriteBatch.draw(bgTexture, 0, 0, worldWidth, worldHeight);
-        bucketSprite.draw(spriteBatch);
-            
+        spriteBatch.draw(bgTexture, 0, 0, WorldWidth, worldHeight);
+        bucketSprite.draw(spriteBatch)
         spriteBatch.end();
     }
     
