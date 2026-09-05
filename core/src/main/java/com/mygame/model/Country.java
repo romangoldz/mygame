0c0f9a548d31b9cbc72499feb482b87ecfa98004
romangoldz <roman.goldzitskiy.2011@gmail.com>
2026-09-05 11:27:55 +0300

    Update Country.java

--- core/src/main/java/com/mygame/model/Country.java
+++ core/src/main/java/com/mygame/model/Country.java
@@ -1,1 +1,20 @@
-.
+package com.mygame.model;
+
+import com.badlogic.gdx.graphics.Color;
+
+public class Country {
+    private String tag;
+    private Color color;
+    private boolean isPlayerControlled;
+    
+    public Country(String tag, Color color) {
+        this.tag = tag;
+        this.color = color;
+        this.isPlayerControlled = false;
+    }
+    
+    public String getTag() { return tag; }
+    public Color getColor() { return color; }
+    public boolean isPlayerControlled() { return isPlayerControlled; }
+    public void setPlayerControlled(boolean controlled) { isPlayerControlled = controlled; }
+}
