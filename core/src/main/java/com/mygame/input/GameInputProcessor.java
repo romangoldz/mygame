46e9e840fceccc0eca4072d8434ee8164313d5c7
romangoldz <roman.goldzitskiy.2011@gmail.com>
2026-09-05 11:38:56 +0300

    Update GameInputProcessor.java

--- core/src/main/java/com/mygame/input/GameInputProcessor.java
+++ core/src/main/java/com/mygame/input/GameInputProcessor.java
@@ -1,66 +1,109 @@
 package com.mygame.input;
 
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.InputProcessor;
+import com.badlogic.gdx.InputAdapter;
 import com.badlogic.gdx.graphics.Camera;
-import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.InputListener;
+import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.game.model.GameMap;
 import com.game.model.Province;
+import com.game.screen.GameScreen;
 
-public class GameInputProcessor implements InputProcessor {
+public class GameInputProcessor extends InputListener {
     private Camera camera;
     private GameMap gameMap;
+    private GameScreen gameScreen;
     private Vector3 touchPos = new Vector3();
-    private float zoomLevel = 1.0f;
+    private Vector3 lastTouch = new Vector3();
+    private boolean isDragging = false;
+    private float pinchZoomStart = 0f;
+    private float pinchDistance = 0f;
     
-    public GameInputProcessor(Camera camera, GameMap gameMap) {
+    // Параметры зума
+    private float minZoom = 0.5f;
+    private float maxZoom = 3.0f;
+    
+    public GameInputProcessor(Camera camera, GameMap gameMap, GameScreen gameScreen) {
         this.camera = camera;
         this.gameMap = gameMap;
+        this.gameScreen = gameScreen;
     }
     
     @Override
-    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-        touchPos.set(screenX, screenY, 0);
-        camera.unproject(touchPos);
-        
-        // Преобразование в координаты карты (0-1)
-        float mapX = touchPos.x / camera.viewportWidth;
-        float mapY = touchPos.y / camera.viewportHeight;
-        
-        Province province = gameMap.getProvinceAt(mapX, mapY);
-        if (province != null) {
-            Gdx.app.log("Province", "ID: " + province.getId() + 
-                       ", Owner: " + province.getOwnerTag());
-            // Здесь можно добавить выделение или открыть UI провинции
+    public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+        if (event.getTarget() != event.getListenerActor()) {
+            return false;
         }
+        
+        touchPos.set(x, y, 0);
+        camera.unproject(touchPos);
+        lastTouch.set(touchPos);
+        isDragging = false;
+        
         return true;
     }
     
     @Override
-    public boolean touchDragged(int screenX, int screenY, int pointer) {
-        // Перемещение камеры
-        Vector3 newPos = new Vector3(screenX, screenY, 0);
+    public void touchDragged(InputEvent event, float x, float y, int pointer) {
+        Vector3 newPos = new Vector3(x, y, 0);
         camera.unproject(newPos);
-        camera.position.add(touchPos.x - newPos.x, touchPos.y - newPos.y, 0);
+        
+        // Перемещение камеры
+        camera.position.add(lastTouch.x - newPos.x, lastTouch.y - newPos.y, 0);
         camera.update();
-        touchPos.set(newPos);
+        
+        lastTouch.set(newPos);
+        isDragging = true;
+    }
+    
+    @Override
+    public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
+        if (!isDragging) {
+            // Это был клик, а не перетаскивание
+            touchPos.set(x, y, 0);
+            camera.unproject(touchPos);
+            
+            // Определение провинции
+            float mapX = touchPos.x / camera.viewportWidth;
+            float mapY = touchPos.y / camera.viewportHeight;
+            
+            Province province = gameMap.getProvinceAt(mapX, mapY);
+            if (province != null) {
+                gameScreen.onProvinceSelected(province);
+            }
+        }
+    }
+    
+    @Override
+    public boolean mouseMoved(InputEvent event, float x, float y) {
+        // Для подсветки провинции при наведении (опционально)
         return true;
     }
     
     @Override
-    public boolean scrolled(float amountX, float amountY) {
-        // Зум колесиком мыши (для тестирования на ПК)
+    public boolean scrolled(InputEvent event, float x, float y, float amountX, float amountY) {
+        // Зум колесиком мыши
         float zoomFactor = 1.1f;
         if (amountY > 0) {
-            zoomLevel *= zoomFactor;
+            camera.zoom /= zoomFactor;
         } else {
-            zoomLevel /= zoomFactor;
+            camera.zoom *= zoomFactor;
         }
-        camera.zoom = 1 / zoomLevel;
+        
+        // Ограничение зума
+        camera.zoom = Math.max(minZoom, Math.min(maxZoom, camera.zoom));
         camera.update();
         return true;
     }
     
-    // Другие методы InputProcessor...
+    // Метод для пинч-зума (будет вызван из GestureDetector)
+    public void zoom(float initialDistance, float currentDistance) {
+        float zoomFactor = initialDistance / currentDistance;
+        camera.zoom *= zoomFactor;
+        camera.zoom = Math.max(minZoom, Math.min(maxZoom, camera.zoom));
+        camera.update();
+    }
 }
