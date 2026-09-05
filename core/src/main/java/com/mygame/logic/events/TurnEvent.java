4e553d3d2ac7754fa7780b2d95f7f66bd601e609
romangoldz <roman.goldzitskiy.2011@gmail.com>
2026-09-05 11:32:49 +0300

    Update TurnEvent.java

--- core/src/main/java/com/mygame/logic/events/TurnEvent.java
+++ core/src/main/java/com/mygame/logic/events/TurnEvent.java
@@ -1,1 +1,15 @@
-.
+package com.mygame.logic.events;
+
+public class TurnEvent {
+    public enum Type { START, END }
+    private Type type;
+    private int turnNumber;
+    
+    public TurnEvent(Type type, int turnNumber) {
+        this.type = type;
+        this.turnNumber = turnNumber;
+    }
+    
+    public Type getType() { return type; }
+    public int getTurnNumber() { return turnNumber; }
+}
